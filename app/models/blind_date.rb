7bb48ee99be608e7ecc4df91895dfe9ae18c3c28
Mauricio Reis <mauricio_reis@hotmail.com>
2023-09-10 01:08:36 +0200

    Add basic validations and relations to employee and blind date

--- app/models/blind_date.rb
+++ app/models/blind_date.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class BlindDate < ApplicationRecord
+  has_and_belongs_to_many :employees
+
+  validates :week, :year, presence: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Employee < ApplicationRecord
+  has_and_belongs_to_many :blind_dates
+
+  validates :name, :department, presence: true
 end
